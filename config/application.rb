9531887ce613716d8ad99f1ae19147fbef95b1b2
dtaniwaki <daisuketaniwaki@gmail.com>
2014-07-14 00:21:54 +0900

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,33 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module RackDevMarkSampleApp
+  class Application < Rails::Application
+    # Enable rack-dev-mark
+    config.rack_dev_mark.enable = !Rails.env.production?
+    # Customize themes if you want to do so
+    # config.rack_dev_mark.theme = [:title, :github_fork_ribbon]
+    # Customize inserted place of the middleware if necessary. 
+    # You can use either insert_before or insert_after
+    # config.rack_dev_mark.insert_after = Rack::Deflater
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+
+    config.middleware.insert_before ActionDispatch::ShowExceptions, Rack::Deflater
+  end
+end
