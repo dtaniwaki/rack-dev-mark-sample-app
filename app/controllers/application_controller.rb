8490b9d7c602f283b643ca8b62e1c08e92e21852
dtaniwaki <daisuketaniwaki@gmail.com>
2014-07-19 22:13:56 +0900

    rack-dev-mark I18n test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_locale
+  
+  private
+
+  def set_locale
+    locale = params[:locale] || cookies[:locale] || I18n.default_locale
+    if locale != cookies[:locale]
+      cookies[:locale] = {value: locale, domain: Settings.cookie_domain}
+    end
+    I18n.locale = locale
+  end
 end
